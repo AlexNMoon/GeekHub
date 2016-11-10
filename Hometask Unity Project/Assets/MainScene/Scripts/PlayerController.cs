af2262f7727f493b4e687a16099cac7007b5cd22
Alexandra <moon_lady_@ukr.net>
2016-11-10 13:07:23 +0200

    My FPS

---
+++ Hometask Unity Project/Assets/MainScene/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    public float rotateSpeed = 20f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float translate = Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime;
+	    float rotate = Input.GetAxis("Horizontal")*rotateSpeed*Time.deltaTime;
+        transform.Translate(0f, 0f, translate);
+	    transform.Rotate(0f, rotate, 0f);
+	}
+
+   
+}
