4e1ffa88f5a04e6af746a7984852a594f0796fb7
Alexandra <moon_lady_@ukr.net>
2016-11-10 21:34:32 +0200

    HT 2 done

---
+++ Hometask Unity Project/Assets/MainScene/Scripts/NewSceneLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewSceneLoad : MonoBehaviour
+{
+
+    void OnTriggerStay(Collider other)
+    {
+        SceneManager.LoadScene("HorrorLightScene");
+    }
+
+}
