adb5c7e774c7ab81883b2f554e59560df1db7214
Alexandra <moon_lady_@ukr.net>
2017-01-25 20:09:00 +0200

    HT4 done

--- Hometask/Assets/Scripts/PinDownCountScript.cs
+++ Hometask/Assets/Scripts/PinDownCountScript.cs
@@ -9,13 +9,42 @@ namespace Assets.Scripts
 
         public Text CountText;
         private int _count;
-       
+        private bool[] counted;
+        public GameObject PinsPrefab;
+        public GameObject BallPrefab;
 
+        void Start()
+        {
+            counted = new bool[10];
+            Restart();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                CountText.text = "0";
+                Restart();
+                Destroy(GameObject.FindGameObjectWithTag("Pins"));
+                Destroy(GameObject.FindGameObjectWithTag("Ball"));
+                GameObject Pins = (GameObject)Instantiate(PinsPrefab, new Vector3(1.82f, 1.19f, -0.9f), Quaternion.identity);
+                GameObject Ball = (GameObject)Instantiate(BallPrefab, new Vector3(1.7f, 1.25f, -1.85f), Quaternion.identity);
+            }
+        }
+        
         void OnTriggerEnter(Collider other)
         {
             if (other.tag == "pin")
                 {
-                    ShowScore();
+                    for (int i = 1; i < 11; i++)
+                    {
+                        if ((other.name == i.ToString()) && (!counted[i-1]))
+                        {
+                            ShowScore();
+                            counted[i - 1] = true;
+                        }
+                    }
                 }
             }
 
@@ -26,6 +55,12 @@ namespace Assets.Scripts
             CountText.text = _count.ToString();
         }
 
-        
+        void Restart()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                counted[i] = false;
+            }
+        }
     }
 }
