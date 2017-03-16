b2860335e009321e3005caca0fa08229e06c6d77
Alexandra <moon_lady_@ukr.net>
2017-03-16 17:04:42 +0200

    Animated ui

---
+++ Hometask/Assets/Scripts/ScrollViewAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class ScrollViewAnimation : MonoBehaviour {
+
+        public Animator Animator;
+        public Text Text;
+        public Text ScrollViewText;
+        public Button Button;
+
+      public void ClickedAnimation(GameObject name)
+        {
+            if (name.GetComponent<Text>().text == "Show more")
+            {
+                switch (Text.text)
+                {
+                    case "Kurt Cobain":
+                        ScrollViewText.text = "Kurt Cobain";
+                        break;
+                    case "Frances Bean Cobain":
+                        ScrollViewText.text = "Frances Bean Cobain";
+                        break;
+                    case "Avril Lavigne":
+                        ScrollViewText.text = "Avril Lavigne";
+                        break;
+                    case "Taylor Momsen":
+                        ScrollViewText.text = "Taylor Momsen ";
+                        break;
+                    default:
+                        ScrollViewText.text = "";
+                        break;
+                }
+                Animator.CrossFade("Scroll View Visible", 0f);
+            }
+            else
+            {
+                Animator.CrossFade("Scroll View Invisible", 0f);
+            }
+        }
+    }
+}
---
+++ Hometask/Assets/Scripts/ShowMoreButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class ShowMoreButton : MonoBehaviour
+    {
+
+        public Button Button;
+        private Animator _animator;
+    
+
+        void Start()
+        {
+            _animator = Button.GetComponent<Animator>();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            _animator.CrossFade("Triggered", 0f);
+        }
+
+    
+
+        void OnTriggerExit(Collider other)
+        {
+            _animator.CrossFade("Normal", 0f);
+        }
+
+    }
+}
